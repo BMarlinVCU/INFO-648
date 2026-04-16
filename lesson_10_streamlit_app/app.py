c93b287229df279b13eb7afec2807c48890ebe28
Ben Marlin <bjmarlin@vcu.edu>
2026-04-16 16:00:15 +0000

    fixed pkle file name

--- lesson_10_streamlit_app/app.py
+++ lesson_10_streamlit_app/app.py
@@ -1,10 +1,32 @@
 import streamlit as st
 import pickle
+import pandas as pd
 
 with open("lesson_10_streamlit_app/iris_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("Lesson 25 App")
+st.write("Enter flower measurements below.")
+
+sepal_length = st.number_input("Sepal length", min_value=0.0, value=5.1)
+sepal_width = st.number_input("Sepal width", min_value=0.0, value=3.5)
+petal_length = st.number_input("Petal length", min_value=0.0, value=1.4)
+petal_width = st.number_input("Petal width", min_value=0.0, value=0.2)
 
 if st.button("Predict"):
-    st.write("Model loaded!")
+    new_flower = pd.DataFrame([{
+        "sepal length (cm)": sepal_length,
+        "sepal width (cm)": sepal_width,
+        "petal length (cm)": petal_length,
+        "petal width (cm)": petal_width
+    }])
+
+    prediction = model.predict(new_flower)[0]
+
+    class_names = {
+        0: "Setosa",
+        1: "Versicolor",
+        2: "Virginica"
+    }
+
+    st.success(f"Prediction: {class_names[prediction]}")
