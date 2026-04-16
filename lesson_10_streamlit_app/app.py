733b81d399b87c5cd01cff48c18f06bfaf48230a
Ben Marlin <bjmarlin@vcu.edu>
2026-04-16 14:25:19 +0000

    Updated title to lesson 23

--- lesson_10_streamlit_app/app.py
+++ lesson_10_streamlit_app/app.py
@@ -4,8 +4,7 @@ import pickle
 with open("your_model.pkl", "rb") as f:
     model = pickle.load(f)
 
-st.title("Lesson 10 App")
+st.title("Lesson 23 App")
 
 if st.button("Predict"):
     st.write("Model loaded!")
-    
