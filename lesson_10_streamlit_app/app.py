88ce0dcaef54fab5299712841b28fccb1c5545f8
Ben Marlin <bjmarlin@vcu.edu>
2026-04-16 15:58:34 +0000

    fixed pkle file name

--- lesson_10_streamlit_app/app.py
+++ lesson_10_streamlit_app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 
-with open("iris_model.pkl", "rb") as f:
+with open("lesson_10_streamlit_app/iris_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("Lesson 25 App")
